b94e4df4dd51274a63b27c5b6efe0f5d8f354e74
nooglersoon <nooglersoon@outlook.co.id>
2023-08-15 08:53:07 +0700

    Initial Commit

---
+++ iGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iGame
+//
+//  Created by Fauzi Achmad B D on 15/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
