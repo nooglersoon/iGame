7c8ca9767e17ba09a9d896ae6e5a9fd752cc841c
nooglersoon <nooglersoon@outlook.co.id>
2023-08-15 08:58:12 +0700

    init project

--- iGame/SceneDelegate.swift
+++ iGame/SceneDelegate.swift
@@ -13,10 +13,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        // 2.
+        let window = UIWindow(windowScene: windowScene)
+        
+        // Set Categories as Root ViewController
+        let rootViewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        navigationController.navigationBar.prefersLargeTitles = true
+        window.rootViewController = navigationController
+        
+        // 4.
+        window.makeKeyAndVisible()
+        
+        // 5.
+        self.window = window
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
